f8b00ddb4fb70b35e90ed384c8407f4866f0fba3
ledong1080 <67459708+ledong1080@users.noreply.github.com>
2025-11-16 19:59:28 +0700

    Cập nhật giao diện + chỉnh sửa style.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // THÊM DÒNG NÀY VÀO
+      base: '/tapsan/', 
+      // --------------------
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
